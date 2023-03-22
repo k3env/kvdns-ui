21ff8a88c01ce040d233fc32acc24a0d60fd8eb4
Sergey Tulonov <me@k3env.site>
2023-03-22 11:43:37 +0800

    feat: change labels in header to fa icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,6 +12,8 @@ import {
 import { createEffect, createStore, restore } from 'effector';
 import { useStore } from 'effector-react';
 import Logo from '../assets/logo.svg';
+import Sun from '../assets/sun-regular.svg';
+import Moon from '../assets/moon-regular.svg';
 import { $currentServer, $servers, fx_useServer } from '../stores/servers';
 
 export function Header(props: {}) {
@@ -21,14 +23,20 @@ export function Header(props: {}) {
   return (
     <HStack pb={12} pt={4} justifyContent={'space-between'}>
       <HStack>
-        <Image w={'48px'} h={'48px'} src={Logo} />
+        <Image w="48px" h="48px" src={Logo} />
         <Heading size={'lg'}>KV:53 UI</Heading>
       </HStack>
       <HStack>
-        <Button onClick={toggleColorMode}>{colorMode === 'dark' ? 'Dark' : 'Light'}</Button>
-        <Select onChange={(e) => fx_useServer(e.target.value)}>
+        <Button onClick={toggleColorMode}>
+          {colorMode === 'dark' ? (
+            <img src={Moon} width="24px" height="24px" color="#ffff00" />
+          ) : (
+            <img src={Sun} width="24px" height="24px" />
+          )}
+        </Button>
+        <Select value={current} onChange={(e) => fx_useServer(e.target.value)}>
           {servers.map((s) => (
-            <option value={s} selected={s === current}>
+            <option value={s} key={s}>
               {s}
             </option>
           ))}
